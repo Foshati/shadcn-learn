782b3aabbef354c5348ce21c6d75852a1b270bd7
Foshati <amirrezafoshati@gmail.com>
2024-08-07 19:52:41 +0330

    create Dropdown

--- src/components/layout/header/Navbar.tsx
+++ src/components/layout/header/Navbar.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import logofa from "/public/img/logofa.png";
 import Link from "next/link";
-import { CircleUserRound } from "lucide-react";
-
+import { CircleUserRound, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export const Navbar = () => {
   return (
     <div className="bg-slate-900 py-3 px-5 flex items-center justify-between">
@@ -17,17 +25,31 @@ export const Navbar = () => {
         />
       </Link>
 
-      <Avatar>
-        <AvatarImage
-          src="https://i.pravatar.cc/300"
-          alt="avatar"
-          width={40}
-          className="rounded-full"
-        />
-        <AvatarFallback>
-          <CircleUserRound className="size-8" />
-        </AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage
+              src="https://i.pravatar.cc/300"
+              alt="avatar"
+              width={40}
+              className="rounded-full"
+            />
+            <AvatarFallback>
+              <CircleUserRound className="size-8" />
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Log out</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
